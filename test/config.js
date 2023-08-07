83bad2612f41985894d3564fe98cfe7c5d7b0fc2
Irae Carvalho <irae@irae.pro.br>
2023-08-07 10:59:44 -0300

    Proper fix for mendel-config tests

--- test/config.js
+++ test/config.js
@@ -13,16 +13,12 @@ var origMendelEnv = process.env.MENDEL_ENV;
 delete process.env.NODE_ENV;
 delete process.env.MENDEL_ENV;
 var where;
-var opts;
 
 mkdirp.sync('/tmp/1/2/3/');
-
-
 process.chdir('/tmp/');
 
 t.match(config('./1/2/3/').basedir, '1/2/3',
     'recurses and give up if no config found');
-
 
 process.chdir(path.resolve(__dirname, './config-samples/2/subfolder/'));
 
@@ -34,7 +30,6 @@ t.match(config(), {
        'json_B': ['folder_B'],
     },
 }, 'find package.json, and match some variations');
-
 
 process.chdir(path.resolve(__dirname));
 
@@ -67,11 +62,10 @@ t.match(config(where), {
     outdir: path.resolve(__dirname, 'config-samples/mendel'),
 }, 'ignores package.json that don\'t contain \'mendel\' entry');
 
-opts = {
+t.match(config({
     outdir: '../build',
     bundleName: 'testBundle',
-};
-t.match(config(opts), {
+}), {
     basedir: path.resolve(__dirname),
     outdir: path.resolve(__dirname, '../build'),
     bundleName: 'testBundle',
@@ -210,11 +204,11 @@ t.match(config(where), {
     outlets: [
         {
             id: 'manifest',
-            plugin: false,
+            plugin: /.+\/mendel-outlet-manifest.*/,
         },
         {
             id: 'css',
-            plugin: false,
+            plugin: /.+\/mendel-outlet-css.*/,
             options: {
                 plugin: [
                     [
