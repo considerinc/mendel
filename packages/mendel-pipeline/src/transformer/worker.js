e7cfda1b41b9b8cafb017d9fefff65199d504d13
Irae Carvalho <irae@irae.pro.br>
2023-08-07 03:40:26 -0300

    Avoid name collision and better debug of rare transform error

--- packages/mendel-pipeline/src/transformer/worker.js
+++ packages/mendel-pipeline/src/transformer/worker.js
@@ -4,25 +4,28 @@ module.exports = function() {
     const debug = require('debug')('mendel:transformer:slave-' + process.pid);
 
     return {
-        start({source, transforms, filename}) {
+        start({source: startSource, transforms, filename}) {
             debug(`[Slave ${process.pid}] Starting transform.`);
-            let promise = Promise.resolve({source});
+            let promise = Promise.resolve({source: startSource});
 
             transforms.forEach(transform => {
+                const xform = require(transform.plugin);
+                if (typeof xform !== 'function') {
+                    throw new Error(`Transform ${transform.id} is incompatible with Mendel.`);
+                }
+
                 promise = promise
                 .then(analytics.tic.bind(analytics, transform.id))
                 .then(({source, map}) => {
-                    const xform = require(transform.plugin);
-
-                    if (typeof xform !== 'function') {
-                        throw new Error(`Transform ${transform.id} is incompatible with Mendel.`);
-                    }
-
                     return xform({filename, source, map}, transform.options);
                 })
                 .then(analytics.toc.bind(analytics, transform.id))
                 .then(result => {
-                    debug(`[Slave ${process.pid}][${transform.id}] "${filename}" transformed`);
+                    if(startSource !== undefined && result.source === undefined) {
+                        debug(`Transform ${transform.id} returned empty result for non empty input`);
+                    } else {
+                        debug(`[Slave ${process.pid}][${transform.id}] "${filename}" transformed`);
+                    }
                     return result;
                 });
             });
