c070805ee2046ed29d53f6623b1289386df65331
Irae Carvalho <irae@irae.pro.br>
2023-09-09 15:02:35 -0300

    test: add coverage map

---
+++ .tap.coverage.map.js
@@ -0,0 +1,22 @@
+// npx tap --coverage-map=.tap.coverage.map.js ./test/some-file.js
+
+const {relative} = require('node:path');
+
+module.exports = function (testFile) {
+    return map[relative(__dirname, testFile)] || [];
+};
+
+const map = {
+    'test/trees.js': ['packages/mendel-core/tree.js'],
+    'test/tree-walker.js': ['packages/mendel-core/tree-walker.js'],
+    'test/tree-serialiser.js': ['packages/mendel-core/tree-serialiser.js'],
+    'test/tree-deserialiser.js': ['packages/mendel-core/tree-deserialiser.js'],
+    'test/tree-hash-walker.js': ['packages/mendel-core/tree-hash-walker.js'],
+    'test/tree-walker.js': ['packages/mendel-core/tree-walker.js'],
+    'test/tree-variation-walker-server.js': [
+        'packages/mendel-core/tree-variation-walker-server.js',
+    ],
+    'test/tree-variation-walker.js': [
+        'packages/mendel-core/tree-variation-walker.js',
+    ],
+};
